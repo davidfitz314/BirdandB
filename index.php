7246bc4818c72eba7cc27853d4996cfe8ff9813e
davidfitz314 <davidfitz314@gmail.com>
2017-02-27 16:42:12 -0700

    changing over to php extensions

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<gead>
+<meta charset="UTF-8">
+  <meta name="description" content="Web Education">
+  <meta name="keywords" content="HTML,CSS,XML,PHP">
+  <meta name="author" content="David ChenFitzgerald">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<h1>BirdandB.com</h1>
+<p>Is curently under construction.</p>
+<p>Please check back with us at a later time. :)</p>
+<p>...</p>
+<p>To take a survey about sexual topics that can be talked about please click the following link<p>
+<iframe src="https://docs.google.com/a/dmail.dixie.edu/forms/d/e/1FAIpQLSfbFvWKJa8v4vdAJ0Cqg8Y8xe-r2-F1QwW5xeh6gGxlhfCcig/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Survey</iframe>
+</body>
+</html>
+<footer>&copy; 2017 <br> David ChenFitzgerald &amp; Jamie Chapman</footer>	
