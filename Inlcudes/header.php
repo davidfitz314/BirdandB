8a370d335b3379df6d96a5ddedbc2fc4c314ba01
davidfitz314 <davidfitz314@gmail.com>
2017-02-27 17:42:21 -0700

    stylesheets are testing

--- Inlcudes/header.php
+++ Inlcudes/header.php
@@ -1,12 +1,13 @@
 <!doctype html>
 <html>
 <head>
- <meta charset="UTF-8">
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="../styles.css">
   <meta name="description" content="Web Education">
   <meta name="keywords" content="HTML,CSS,XML,PHP,JavaScript">
   <meta name="author" content="David ChenFitzgerald">
   <meta name="author" content="Jamie Chapmans">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="../styles.css">
 </head>
+<header>test</header>
 
