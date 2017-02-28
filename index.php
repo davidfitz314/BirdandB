f5bececfe1cbeecc4069c36bcc654ff1b7e941f6
davidfitz314 <davidfitz314@gmail.com>
2017-02-27 20:49:27 -0700

    updating css

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	<p><strong>Learning About Life Never Stops</strong></p>
 	<p>Hate trying to understand things about your body or sexuality that everyone else seems to know already? Well we did too, and that's why we developed this app and website to provide you with the knowledge that others forgot to tell you.<p>	
 	</br>
-	<p>To take a survey about sexual topics that can be talked about please complete the following form<p>
+	<p>To take a survey on sexual topics that can be talked about please complete the following form<p>
 	<iframe src="https://docs.google.com/a/dmail.dixie.edu/forms/d/e/1FAIpQLSfbFvWKJa8v4vdAJ0Cqg8Y8xe-r2-F1QwW5xeh6gGxlhfCcig/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Survey</iframe>
 	</br>
 	<?php include 'includes/footer.php'; ?>
