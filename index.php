26b94b5747de1abc3cf8e26b63cbc217fcf44130
davidfitz314 <davidfitz314@gmail.com>
2017-02-27 17:57:49 -0700

    updates footer

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once('/Includes/header.php') ?>
+<?php include_once('/Includes/header.php') ?>
 
 <h1>BirdandB.com</h1>
 <p>Is curently under construction.</p>
@@ -7,7 +7,7 @@
 <p>To take a survey about sexual topics that can be talked about please complete the following form<p>
 <iframe src="https://docs.google.com/a/dmail.dixie.edu/forms/d/e/1FAIpQLSfbFvWKJa8v4vdAJ0Cqg8Y8xe-r2-F1QwW5xeh6gGxlhfCcig/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Survey</iframe>
 
-<?php require_once('Includes/footer.php') ?>
+<?php include_once('Includes/footer.php') ?>
 
 </body>
 </html>
