8e909559c24a4b49b003d0ac7d16525acea7d199
davidfitz314 <davidfitz314@gmail.com>
2017-02-27 17:43:23 -0700

    stylesheets are testing

--- Inlcudes/footer.php
+++ Inlcudes/footer.php
@@ -1,2 +1,3 @@
+</body>
 <footer>&copy; 2017 <br> David ChenFitzgerald &amp; Jamie Chapman</footer>	
 </html>
--- Inlcudes/header.php
+++ Inlcudes/header.php
@@ -10,4 +10,4 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <header>test</header>
-
+<body>
