3e4f0537267aa53377d21704200b1fcf2924fddd
davidfitz314 <davidfitz314@gmail.com>
2017-03-02 13:30:00 -0700

    adding hover options to cta link

--- includes/factsfunc.php
+++ includes/factsfunc.php
@@ -14,7 +14,7 @@ function pickRandom() {
 	"Interesting fact: at week 27 the baby’s lungs aren’t fully formed, but if an early birth occurs the lungs can still function with medical help.",
 	"Interesting Fact: The average baby’s mother gains about 1lb a week at this time but ½ of that goes to the baby.",
 	"Interesting Fact: The baby’s skull won’t be fully fused until adulthood this allows the baby’s head to squeeze through the birth canal.",
-	/* chapter 3 here */
+	/* chapter 3 here prior count is 11 */
 	"Interesting Fact: Many children will have growing pains during their life until they reach adulthood. To ease these symptoms many people treat them like sports injuries, applying warm heat to the painful area, stretching and gently massaging it to make the pain go away.",
 	"Interesting Fact: if a male begins puberty before the age of 9 or doesn’t show signs of puberty by the age of 14 it is recommend that they see a pediatrician and have a full examination.",
 	"Interesting Fact: In most males one testicle, usually the left side, is slightly lower than the other testicle. Many scientist believe that this happens to protect the testicles from bouncing into one another while the male is moving around.",
@@ -26,12 +26,12 @@ function pickRandom() {
 	"Interesting Fact: Many young females have misconceptions about menstruation and the best time to ease their worries about it is when they have first begun developing breast buds as this signals menstruation and more is coming. Always remember that menstruation is different for every female, the amount of cramps, pain and other experiences vary for each individual.",
 	"Interesting Fact: A doctor should be called if a sudden unexplainable change happens in the female's period such as: heavy bleeding that soaks through more that 6 - 8 pads a day, bleeding between periods, period lasting longer than 7 - 10 days, and sudden development of severe abdominal pain.",
 	"Interesting Fact: Beginning with puberty many females start to become interested in love and acting it out with other people, this can be scary for their parents, but should be treated calmly and explained thoroughly by their parents or legal guardians. If needed outside resources are available online that can be used as tools for teaching young females."
-	/* end chapter 3 */
+	/* end chapter 3 prior count is 11*/
 );
 	$output = $input[array_rand($input)];
 	echo $output;
-	echo "__________";
-	echo array_search($output, $input);
+	/*echo "__________"; use \/ to get the location from the array
+	echo array_search($output, $input);*/
 }
 
 ?>
