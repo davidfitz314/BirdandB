bb595ccd737b5674209db2838c14b0087eb7d848
davidfitz314 <davidfitz314@gmail.com>
2017-03-01 17:50:44 -0700

    homepage style updates logo

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
+<!- continued from header centered-wrapper div ->
 <?php include 'includes/headb.php'; ?>
 
 <!- Company describtion section ->
 <div class="CompanyDescription">
 	<p><strong>Learning About Life Never Stops</strong></p>
 	<p>Hate trying to understand things about your body or sexuality that everyone else seems to know already? Well we did too, and that's why we developed this app and website to provide you with the knowledge that others forgot to tell you.<p>	
-</div>
+</div><!- end CompanyDescription div ->
 
 <!- first set of calls to action ->
 <div class="CallsToAction">
 <div class="CTA1"><p>call to action 1</p></div>
 <div class="CTA2"><p>call to action 2</p></div>
-
-</div>
+</div><!- end CallsToAction div ->
 
 <!- Survey section ->
 <div class="Survey">
 	<p>To take a survey on sexual topics that can be talked about please complete the following form<p>
 	<iframe src="https://docs.google.com/a/dmail.dixie.edu/forms/d/e/1FAIpQLSfbFvWKJa8v4vdAJ0Cqg8Y8xe-r2-F1QwW5xeh6gGxlhfCcig/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Survey</iframe>
-</div>
+</div><!- end Survey div ->
 
 <!- Footer section ->
 <?php include 'includes/footer.php'; ?>
 
-</div>
+</div><!- end centered-wrapper div ->
 </body>
 </html>
