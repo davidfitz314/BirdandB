d5e4a00100c1487f72800042087e7ce34f31f888
davidfitz314 <davidfitz314@gmail.com>
2017-03-01 18:39:46 -0700

    homepage style updates logo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!- continued from header centered-wrapper div ->
 <?php include 'includes/headb.php'; ?>
+<?php require_once('includes/facsfunc.php'); ?>
 
 <!- Company describtion section ->
 <div class="CompanyDescription">
@@ -17,9 +18,7 @@
 <!- end CTA1 div ->
 
 <div class="CTA2"><p>
-<?php require_once('includes/facsfunc.php'); 
-	pickRandom();
-?>
+<?php pickRandom(); ?>
 </p></div>
 <!- end CTA2 div ->
 </div>
