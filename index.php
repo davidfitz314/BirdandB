95ed2db09a77e9cf5ae1593268e5b522669dae06
davidfitz314 <davidfitz314@gmail.com>
2017-03-01 19:27:18 -0700

    homepage cta func for randomizing info

--- index.php
+++ index.php
@@ -6,9 +6,17 @@
 <!- Company describtion section ->
 <div class="CompanyDescription">
 	<p><strong>Learning About Life Never Stops</strong></p>
-	<p>Hate trying to understand things about your body or sexuality that everyone else seems to know already? Well we did too, and that's why we developed this app and website to provide you with the knowledge that others forgot to tell you.<p>	
+	<p>Hate trying to understand things about your body or sexuality that everyone else seems to know already? Well we did too, and that's why we developed this app and website to provide you with the knowledge that others forgot to tell you.<p>
+</br><p>We are currently under construction, please come again soon.<p>	
 </div>
 <!- end CompanyDescription div ->
+
+<!- Survey section ->
+<div class="Survey">
+	<p>To take a survey on sexual topics that can be talked about please complete the following form<p>
+	<iframe src="https://docs.google.com/a/dmail.dixie.edu/forms/d/e/1FAIpQLSfbFvWKJa8v4vdAJ0Cqg8Y8xe-r2-F1QwW5xeh6gGxlhfCcig/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Survey</iframe>
+</div>
+<!- end Survey div ->
 
 <!- first set of calls to action ->
 <div class="CallsToAction">
@@ -25,13 +33,6 @@ CTA goes here!
 <!- end CTA2 div ->
 </div>
 <!- end CallsToAction div ->
-
-<!- Survey section ->
-<div class="Survey">
-	<p>To take a survey on sexual topics that can be talked about please complete the following form<p>
-	<iframe src="https://docs.google.com/a/dmail.dixie.edu/forms/d/e/1FAIpQLSfbFvWKJa8v4vdAJ0Cqg8Y8xe-r2-F1QwW5xeh6gGxlhfCcig/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Survey</iframe>
-</div>
-<!- end Survey div ->
 
 <!- Footer section ->
 <?php include 'includes/footer.php'; ?>
