93b79bb559651476e430abe36b068ba73d472c48
davidfitz314 <davidfitz314@gmail.com>
2017-03-13 16:51:14 -0600

    image upload

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <div class="CallsToAction">
 
 	<div class="CTA1"><a href="<?= "index.php" ?>">
-	<img src='includes/images/lightOFF.png' width='125px' onmouseover="this.src='includes/images/lightOFF.png';" onmouseout="this.src='includes/images/lightON.png';" />
+	<img src='includes/images/lightOFF.png' width='125px' onmouseover="this.src='includes/images/lightON.png';" onmouseout="this.src='includes/images/lightOFF.png';" />
 	
 	<p><strong><?php pickRandom(); ?></strong><p>
 	</a></div>
