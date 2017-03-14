2e1e2d1141662be7b8d72871fae324755095c971
davidfitz314 <davidfitz314@gmail.com>
2017-03-14 16:54:17 -0600

    modifying chapter 2

---
+++ chapter3.php
@@ -0,0 +1,62 @@
+<!- continued from header centered-wrapper div ->
+<?php include 'includes/chapHeader.php'; ?>
+
+<!- begin chapter 2 ->
+
+<!- navigation side bar ->
+<nav class="floating-menu">
+<h1>Chapter 3: Default </h1>
+<strong><a href="chapter1.php">ch1 underConstruction</a></strong>
+<strong><a href="chapter2.php">ch2: Conception</a></strong>
+<ul>
+<strong>Chapter 3</strong>
+<li>
+<a href="chapter3.php">default</a>
+</li>
+<li>
+<a href="chapter3.php">default</a>
+</li>
+<li>
+<a href="chapter3.php">default</a>
+</li>
+<li>
+<a href="chapter3.php">default</a>
+</li>
+</ul>
+<strong><a href="chapter4.php">ch4 underConstruction</a></strong>
+<strong><a href="chapter5.php">ch5 underConstruction</a></strong>
+<strong><a href="chapter6.php">ch6 underConstruction</a></strong>
+<strong><a href="chapter7.php">ch7 underConstruction</a></strong>
+<strong><a href="chapter8.php">ch8 underConstruction</a></strong>
+<strong><a href="chapter9.php">ch9 underConstruction</a></strong>
+<strong><a href="chapter10.php">ch10 underConstruction</a></strong>
+<strong><a href="chapter11.php">ch11 underConstruction</a></strong>
+<strong><a href="chapter12.php">ch12 underConstruction</a></strong>
+<strong><a href="chapter13.php">ch13 underConstruction</a></strong>
+<strong><a href="chapter14.php">ch14 underConstruction</a></strong>
+<strong><a href="chapter15.php">ch15 underConstruction</a></strong>
+</nav>
+<!- end navigation sidebar ->
+
+<!- top page image ->
+<div class="chapterLogo">
+<img src="includes/images/medical_instruments.jpg" alt="change to default">
+</div>
+
+<!- chapter text division ->
+<div class="chapter2Text">
+<br>
+<strong><p>Under construction </p></strong>
+</div>
+
+<div class="expandtest">
+</div>
+
+
+<!- Footer section ->
+<?php include 'includes/footer.php'; ?>
+
+</div>
+<!- end centered-wrapper div ->
+</body>
+</html>
