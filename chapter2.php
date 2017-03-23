a272891f0a9d7bda38759c3f1f4811ff5865ba49
davidfitz314 <davidfitz314@gmail.com>
2017-03-23 14:02:47 -0600

    updating chapter 2 image layout

--- chapter2.php
+++ chapter2.php
@@ -85,8 +85,10 @@
 	<img src="includes/images/chapter2/sesameSeed.png" alt="sesame seed" style="width:64px;height:35px">	
 	</div>
 
+	<div class="weekNum">	
+	<p>5 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>5 weeks</h4>
 	<p>The baby’s appearance is more similar to a tadpole then a human at this point but it is growing fast. The heart will begin beating this week and a circulatory system will start to form. The baby is the size of a sesame seed.</p>
 	</div>
 
@@ -95,8 +97,10 @@
 	<img src="includes/images/chapter2/lentil.png" alt="lentil" style="width:64px;height:50px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>6 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>6 weeks</h4>
 	<p>The baby’s facial features (nose, mouth, and ears), brain and intestines are beginning to develop. The baby is now the size of a lentil.</p>
 	</div>
 
@@ -105,8 +109,10 @@
 	<img src="includes/images/chapter2/blueberry.png" alt="blue berry" style="width:58px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>7 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>7 weeks</h4>
 	<p>The baby still has a tail but its size has doubled since last week and the tail will soon disappear. Arms and legs begin developing alongside of hands and feet which resemble paddles in shape. The baby is now the size of a blueberry.</p>
 	</div>
 
@@ -118,8 +124,10 @@
 	<img src="includes/images/chapter2/kidneyBean.png" alt="kidneybean" style="width:64px;height:31px">	
 	</div>
 		
-	<div class="ch2text">	
-	<h4>8 weeks</h4>
+	<div class="weekNum">	
+	<p>8 weeks<p>
+	</div>
+	<div class="ch2text">
 	<p>The baby begins moving around but not enough for the mother to feel it yet. Primitive neural pathways are formed from Nerve cells branching out. The lungs have begun developing alongside of breathing tubes in the throat. The baby is now the size of a kidney bean or about ½ an inch long.</p>
 	</div>
 
@@ -128,8 +136,10 @@
 	<img src="includes/images/chapter2/grape.png" alt="grape" style="width:54px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>9 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>9 weeks</h4>
 	<p>The baby’s embryonic tail has disappeared and most of it’s basic physiology is in place (earlobes, nose, mouth, and more). The baby’s weight is a fraction of an ounce and is the size of a grape.</p>
 	</div>
 
@@ -138,8 +148,10 @@
 	<img src="includes/images/chapter2/kumquat.png" alt="kumquat" style="width:64px;height:47px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>10 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>10 weeks</h4>
 	<p>The baby has translucent skin and limbs that can bend, finer details like nails are starting to form. The baby is the size of a kumquat.</p>
 	</div>
 
@@ -148,8 +160,10 @@
 	<img src="includes/images/chapter2/fig.png" alt="fig" style="width:60px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>11 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>11 weeks</h4>
 	<p>The baby has almost fully formed, movement from limbs stretching and kicking are now happening. The diaphragm is developing and can even produce hiccups. The baby is the size of a fig.</p>
 	</div>
 
@@ -158,8 +172,10 @@
 	<img src="includes/images/chapter2/lime.png" alt="lime" style="width:14px;height:11px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>12 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>12 weeks</h4>
 	<p>The baby’s reflexes begin working, fingers and toes will open and close and the baby’s mouth will make sucking movements. At this time the baby can feel a poke from the outside of the stomach, but the mother will not be able to feel the baby’s movements yet. The baby is the size of a lime.</p>
 	</div>
 
@@ -168,8 +184,10 @@
 	<img src="includes/images/chapter2/peas.png" alt="pea pod" style="width:65px;height:36px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>13 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>13 weeks</h4>
 	<p>The baby’s fingers now have fingerprints and the veins and organs can clearly be seen through the skin. The baby is the length of a pea pod.</p><!- interesting fact goes here ->
 	</div>
 
@@ -179,8 +197,10 @@
 	<img src="includes/images/chapter2/lemon.png" alt="lemon" style="width:64px;height:48px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>14 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>14 weeks</h4>
 	<p>The baby can now use facial muscles, brain impulses have begun firing, and the babies kidney are now working. The baby is the size of a lemon.</p><!- interesting fact goes here ->
 	</div>
 
@@ -189,8 +209,10 @@
 	<img src="includes/images/chapter2/apple.png" alt="apple" style="width:46px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>15 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>15 weeks</h4>
 	<p>The baby can sense light but the eyelids are still fused shut. At this week ultrasounds may reveal the sex of the baby and the baby is now the size of an apple.</p>
 	</div>
 
@@ -199,8 +221,10 @@
 	<img src="includes/images/chapter2/avocado.png" alt="avocado" style="width:41px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>16 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>16 weeks</h4>
 	<p>The baby’s hair isn’t visible yet but patterning on the baby scalp has begun for it to start growing. The baby’s legs are more developed and moving, the head is more upright and the ears are almost in their final position. The baby is the size of an avocado.</p>
 	</div>
 
@@ -209,8 +233,10 @@
 	<img src="includes/images/chapter2/turnip.png" alt="turnip" style="width:64px;height:48px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>17 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>17 weeks</h4>
 	<p>The baby’s formerly soft cartilage is now hardening into bone, the umbilical chord is growing stronger and thicker and the baby can now move its joints. The baby is the size of a turnip.</p>
 	</div>
 
@@ -222,8 +248,10 @@
 	<img src="includes/images/chapter2/bellPepper.png" alt="bell pepper" style="width:43px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>18 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>18 weeks</h4>
 	<p>The baby is forming an internal protective coating of myelin around its nerves and the baby’s body is more prone to moving around. The baby is now the size of a bell pepper.</p>
 	</div>
 
@@ -232,8 +260,10 @@
 	<img src="includes/images/chapter2/tomato.png" alt="heirloom tomato" style="width:64px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>19 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>19 weeks</h4>
 	<p>The baby is developing its sense of smell, vision, taste, touch, and hearing now. The baby is now the size of a heirloom tomato</p>
 	</div>
 
@@ -242,8 +272,10 @@
 	<img src="includes/images/chapter2/banana.png" alt="banana" style="width:64px;height:36px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>20 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>20 weeks</h4>
 	<p>The baby’s digestive system is now producing Meconium (an early form of poop). The baby is now the size of a banana.</p>
 	</div>
 
@@ -252,8 +284,10 @@
 	<img src="includes/images/chapter2/carrot.png" alt="carrot" style="width:21px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>21 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>21 weeks</h4>
 	<p>The baby’s movement has changed and becomes stronger with kicks and jabs against the walls of the womb. The baby is the length of a carrot.</p><!- interesting fact goes here ->
 	</div>
 
@@ -262,8 +296,10 @@
 	<img src="includes/images/chapter2/spaghettiSquash.png" alt="spaghetti squash" style="width:64px;height:42px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>22 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>22 weeks</h4>
 	<p>The baby’s features are now more distinct, such as lips and eyebrows have more definition. The baby is now the size of a spaghetti squash.</p>
 	</div>
 
@@ -272,8 +308,10 @@
 	<img src="includes/images/chapter2/mango.png" alt="mango" style="width:50px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>23 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>23 weeks</h4>
 	<p>The baby is now the size of a large mango.</p><!- interesting fact goes here ->
 	</div>
 
@@ -282,8 +320,10 @@
 	<img src="includes/images/chapter2/corn.png" alt="corn" style="width:45px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>24 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>24 weeks</h4>
 	<p>The baby is long and slim in its figure but the skin is still thin and translucent. The baby is now the size of an ear of corn.</p>
 	</div>
 
@@ -292,8 +332,10 @@
 	<img src="includes/images/chapter2/rutabaga.png" alt="rutabaga" style="width:64px;height:50px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>25 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>25 weeks</h4>
 	<p>The baby is starting to fill in its wrinkly skin with baby fat and resembling more of a newborn. The baby’s hair is starting to come in and have both color and texture. The baby now weighs around the same as an average rutabaga.</p>
 	</div>
 
@@ -302,8 +344,10 @@
 	<img src="includes/images/chapter2/scallions.png" alt="scallions" style="width:14px;height:11px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>26 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>26 weeks</h4>
 	<p>The baby inhales and exhales amniotic fluid, this helps develop the lungs in preparation for birth when the baby will have to breathe air. The baby is now the size of a bunch of scallions.</p>
 	</div>
 
@@ -312,19 +356,23 @@
 	<img src="includes/images/chapter2/cauliflower.png" alt="cauliflower" style="width:65px;height:58px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>27 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>27 weeks</h4>
 	<p>The baby has now developed a circadian rhythm for sleeping and its brain is very active. The baby is now the size of a head of cauliflower.</p><!- interesting fact goes here ->
 	</div>
 
 	<h3>3rd Trimester</h3>
+	<!- image and week# ->
 	<div class="plant_circle">
 	<img src="includes/images/chapter2/eggplant.png" alt="eggplant" style="width:63px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>28 weeks<p>
+	</div>
 	<div class="ch2text">
-	<!- image and week# ->
-	<h4>28 weeks</h4>
 	<p>The baby has started developing eyesight allowing more light recognition, it’s eyelids can now blink and the eyelashes are fully grown. The baby is now the size of a large eggplant.</p>
 	</div>
 
@@ -333,8 +381,10 @@
 	<img src="includes/images/chapter2/butternutSquash.png" alt="butternut squash" style="width:36px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>29 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>29 weeks</h4>
 	<p>The baby’s head is growing larger for it’s developing brain, muscles and lungs are preparing for life outside the womb. The baby is now the size of a butternut squash.</p>
 	</div>
 
@@ -343,8 +393,10 @@
 	<img src="includes/images/chapter2/cabbage.png" alt="cabbage" style="width:65px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>30 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>30 weeks</h4>
 	<p>The baby is still growing there is only 1 and ½ pints of amniotic fluid surrounding it now. The baby is the size of a large cabbage.</p>
 	</div>
 
@@ -353,8 +405,10 @@
 	<img src="includes/images/chapter2/coconut.png" alt="coconut" style="width:65px;height:63px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>31 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>31 weeks</h4>
 	<p>The baby now has enough control and strength to move its head from side to side, a layer of protective fat has developed throughout its body. The baby is now the size of a coconut.</p>
 	</div>
 
@@ -363,8 +417,10 @@
 	<img src="includes/images/chapter2/jicama.png" alt="jicama" style="width:63px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>32 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>32 weeks</h4>
 	<p>The baby is now going to start gaining ⅓ to ½ of its birth weight in the next seven weeks. The baby is now the size of a large jicama.</p><!- interesting fact goes here ->
 	</div>
 
@@ -373,8 +429,10 @@
 	<img src="includes/images/chapter2/pineapple.png" alt="pineapple" style="width:24px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>33 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>33 weeks</h4>
 	<p>The baby is now the size of a pineapple.</p><!- interesting fact goes here ->
 	</div>
 
@@ -383,8 +441,10 @@
 	<img src="includes/images/chapter2/cantaloupe.png" alt="cantaloupe" style="width:64px;height:63px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>34 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>34 weeks</h4>
 	<p>The baby’s lungs and central nervous system are maturing. The baby can now be born as long as there are no other health problems. The baby is now the size of a cantaloupe.</p>
 	</div>
 
@@ -393,8 +453,10 @@
 	<img src="includes/images/chapter2/honeydew.png" alt="honeydew melon" style="width:64px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>35 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>35 weeks</h4>
 	<p>The baby’s kidneys are now fully developed and the liver can process some waste products. The baby is now the size of a honeydew melon.</p>
 	</div>
 
@@ -403,8 +465,10 @@
 	<img src="includes/images/chapter2/romaineLettuce.png" alt="romain lettuce" style="width:64px;height:60px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>36 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>36 weeks</h4>
 	<p>The baby now gains about 1 once each day in weight. The Vernix Casosa, the waxy substance that used to protect its skin, is being shed. The baby is now the size of a head of romaine lettuce.</p>
 	</div>
 
@@ -413,8 +477,10 @@
 	<img src="includes/images/chapter2/swissChard.png" alt="swiss chard" style="width:65px;height:36px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>37 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>37 weeks</h4>
 	<p>The baby’s lungs and brains begin their final maturation phase. The baby is now the size of a Swiss chard.</p>
 	</div>
 
@@ -423,19 +489,23 @@
 	<img src="includes/images/chapter2/leek.png" alt="leek" style="width:24px;height:64px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>38 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>38 weeks</h4>
 	<p>The baby is now the size of a leek.</p>
 	</div>
 
 	<h3>Turning Full Term</h3>
+	<!- image and week# ->
 	<div class="plant_circle">
 	<img src="includes/images/chapter2/watermelon.png" alt="watermelon" style="width:51px;height:47px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>39 weeks<p>
+	</div>
 	<div class="ch2text">
-	<!- image and week# ->
-	<h4>39 weeks</h4>
 	<p>The baby’s physical developments are complete and the baby is considered full term. The baby is now the size of a mini watermelon.</p>
 	</div>
 
@@ -444,8 +514,10 @@
 	<img src="includes/images/chapter2/pumpkin.png" alt="pumpkin" style="width:64px;height:46px">	
 	</div>
 		
+	<div class="weekNum">	
+	<p>40 weeks<p>
+	</div>
 	<div class="ch2text">
-	<h4>40 weeks</h4>
 	<p>The baby is now the size of a small pumpkin.</p>
 	</div>	
 
