11d491405940779f2747b95e2046b54dc2a3682c
davidfitz314 <davidfitz314@gmail.com>
2017-03-28 19:54:52 -0600

    updating chapter 3

--- chapter2.php
+++ chapter2.php
@@ -22,20 +22,19 @@
 <a href="chapter2.php">Turning Full Term</a>
 </li>
 </ul>
-<strong><a href="chapter3.php">ch3 underConstruction</a>
-<strong><a href="chapter4.php">ch4 underConstruction</a>
-<strong><a href="chapter5.php">ch5 underConstruction</a>
-<strong><a href="chapter6.php">ch6 underConstruction</a>
-<strong><a href="chapter7.php">ch7 underConstruction</a>
-<strong><a href="chapter8.php">ch8 underConstruction</a>
-<strong><a href="chapter9.php">ch9 underConstruction</a>
-<strong><a href="chapter10.php">ch10 underConstruction</a>
-<strong><a href="chapter11.php">ch11 underConstruction</a>
-<strong><a href="chapter12.php">ch12 underConstruction</a>
-<strong><a href="chapter13.php">ch13 underConstruction</a>
-<strong><a href="chapter14.php">ch14 underConstruction</a>
-<strong><a href="chapter15.php">ch15 underConstruction</a>
-</strong>
+<strong><a href="chapter3.php">ch3 underConstruction</a></strong>
+<strong><a href="chapter4.php">ch4 underConstruction</a></strong>
+<strong><a href="chapter5.php">ch5 underConstruction</a></strong>
+<strong><a href="chapter6.php">ch6 underConstruction</a></strong>
+<strong><a href="chapter7.php">ch7 underConstruction</a></strong>
+<strong><a href="chapter8.php">ch8 underConstruction</a></strong>
+<strong><a href="chapter9.php">ch9 underConstruction</a></strong>
+<strong><a href="chapter10.php">ch10 underConstruction</a></strong>
+<strong><a href="chapter11.php">ch11 underConstruction</a></strong>
+<strong><a href="chapter12.php">ch12 underConstruction</a></strong>
+<strong><a href="chapter13.php">ch13 underConstruction</a></strong>
+<strong><a href="chapter14.php">ch14 underConstruction</a></strong>
+<strong><a href="chapter15.php">ch15 underConstruction</a></strong>
 </nav>
 <!- end navigation sidebar ->
 
