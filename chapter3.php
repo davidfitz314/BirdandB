cab3a1deab99ee59d41fb33ef4b0077e043eae0f
davidfitz314 <davidfitz314@gmail.com>
2017-03-28 18:40:27 -0600

    updating chapters

--- chapter3.php
+++ chapter3.php
@@ -5,22 +5,19 @@
 
 <!- navigation side bar ->
 <nav class="floating-menu">
-<h1>Chapter 3: Default </h1>
+<h1>Chapter 3: Human Body Development </h1>
 <strong><a href="chapter1.php">ch1 underConstruction</a></strong>
 <strong><a href="chapter2.php">ch2: Conception</a></strong>
 <ul>
 <strong>Chapter 3</strong>
 <li>
-<a href="chapter3.php">default</a>
+<a href="chapter3.php">Pre-Puberty Changes</a>
 </li>
 <li>
-<a href="chapter3.php">default</a>
+<a href="chapter3.php">Puberty for Males</a>
 </li>
 <li>
-<a href="chapter3.php">default</a>
-</li>
-<li>
-<a href="chapter3.php">default</a>
+<a href="chapter3.php">Puberty for Females</a>
 </li>
 </ul>
 <strong><a href="chapter4.php">ch4 underConstruction</a></strong>
@@ -40,18 +37,13 @@
 
 <!- top page image ->
 <div class="chapterLogo">
-<img src="includes/images/medical_instruments.jpg" alt="change to default">
+<img src="includes/images/chapter3/growth.png" alt="male and female growth">
 </div>
 
 <!- chapter text division ->
-<div class="chapter2Text">
-<br>
+<div class="chapter3Text">
 <strong><p>Under construction </p></strong>
 </div>
-
-<div class="expandtest">
-</div>
-
 
 <!- Footer section ->
 <?php include 'includes/footer.php'; ?>
