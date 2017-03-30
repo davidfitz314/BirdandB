d55d82edeeebbd6ec7c2572dc0c778112a161aa7
davidfitz314 <davidfitz314@gmail.com>
2017-03-29 19:32:24 -0600

    updating chapter 4s content

--- chapter4.php
+++ chapter4.php
@@ -41,18 +41,92 @@
 
 <!- top page image ->
 <div class="chapterLogo">
-<img src="includes/images/medical_instruments.jpg" alt="change to default">
+<img src="includes/images/chapter4/faces(bounce).png" alt="Multiple Emoticons and Types">
 </div>
 
 <!- chapter text division ->
-<div class="chapter2Text">
+<div class="chapter4Text">
 <br>
-<strong><p>Under construction </p></strong>
-</div>
+<h2>Chapter 4: Human Nature & Psychological Dependencies</h2>
+	<p>Every human is a mammal, but what appears to make us different from other mammals is our comprehension of self and our emotions. When one talks about animal nature they are usually discussing base desires for reproducing, eating, and self defense, but when one talks about Human nature they discuss it in a more philosophical manner.</p>
+	<!- Interesting Fact goes here ->
 
-<div class="expandtest">
-</div>
+	<h4>Human Nature</h4>
+	<p>Human nature is defined as: referring to distinguishing characteristics of thinking feeling and acting. “I think, therefore I am” is a famous saying from the philosopher Rene Descartes, it is used often in the study of the mind but came into western culture as a building block for foundation of knowledge in the face of doubt. It describes human thoughts as always looking for a foundation to build upon and trying hard to resist the doubts from the inner and outer self.</p>
+	<p>Human nature deals with emotions as a core foundation point for thoughts and actions. How you feel decides what actions you take, Tracee Ellis Ross an American Actress once said “I need to see my own beauty and to continue to be reminded that I am enough, that I am worthy of love without effort, that I am beautiful, that the texture of my hair and that the shape of my curves, the size of my lips, the color of my skin, and the feelings that I have are all worthy and okay.” Her point shows that without positive emotions it was hard to continue to act when it came to love.</p>
+	<p>Human nature also closely studies the actions taken by humans, Mahatma Gandhi an Indian civil rights leader and well known figure for peace once said “Your beliefs become your thoughts, Your thoughts become your words, Your words become your actions, Your actions become your habits, Your habits become your values, Your values become your destiny.” This shows that after your emotions and thoughts come together they become words and actions which tells the world who you are.</p>
+	<!- Interesting Fact goes here ->
 
+	<h4>Emotions</h4>
+	<p>It is currently believed that humans have 4 to 8 base emotions that when combined make up all known emotions. A famous psychologist Robert Plutchik after many years of study put together the Plutchik Model for emotions, which is commonly used today. The model consist of 8 different emotions -Disgust, Anger, Anticipation, Joy, Acceptance, Fear, Surprise, and Sadness.- and how they combine with the emotions closest to them:</p>
+	<ul class="solidCircle">
+		<li>Disgust: combines with Anger to create Contempt or combines with Sadness to create Remorse.</li>
+
+		<li>Anger: combines with Anticipation to create Aggressiveness or combines with disgust to create Contempt.</li>
+	<!- Interesting Fact goes here ->
+		<li>Anticipation: combines with Joy to create Optimism or combines with Anger to create Aggressiveness.</li>
+
+		<li>Joy: combines with Acceptance to create Love or combines with Anticipation to create Optimism.</li>
+
+		<li>Acceptance: combines with Fear to create Submission or combines with Joy to create Love.</li>
+
+		<li>Fear: combines with Surprise to create Awe or combines with Acceptance to create Submission.</li>
+
+		<li>Surprise: combines with Sadness to create Disappointment or combines with Fear to create Awe.</li>
+
+		<li>Sadness: combines with Disgust to create Remorse or combines with Surprise to create Disappointment.</li>
+	</ul>		
+	<!- Interesting Fact goes here ->
+
+	<h4>Child Behavior and Attachments</h4>
+	<p>Even during childhood humans are studied to understand their nature. One area focused on by Mary Ainsworth -a Canadian-American Psychologist- is called Attachment Theory and relates to a child’s attachment type to its parents. The theory behind it goes into details saying how children during early stages of their lives learn how they should behave or be dependent on others, which then becomes the basis for their adult selves behavior attachments.</p>
+	
+	<p>The 4 main categories for child attachment are Secure, Avoidant, Ambivalent, Disorganized. The behaviors of both the child and caregiver for these attachments are given below.</p>
+
+	<ul class="solidCircle">
+		<li>Secure from Caregiver: The caregiver is responsive, quick and positive towards the child’s needs.</li>
+
+		<li>Secure from Child: The child is Distressed when caregiver leaves, happy when caregiver returns, and seeks comfort from the caregiver when scared or sad.</li>	
+	<!- Interesting Fact goes here ->
+
+		<li>Avoidant from Caregiver: The caregiver is unresponsive, uncaring or dismissive towards the child.</li>
+
+		<li>Avoidant from Child: The child does not acknowledge the caregiver arriving or leaving and does not seek or make contact with caregiver.</li>
+
+		<li>Ambivalent from Caregiver: The caregiver responds inconsistently to the child.</li>
+
+		<li>Ambivalent from Child: The child is distressed when the caregiver leaves but feels no comfort by the return of the caregiver.</li>
+
+		<li>Disorganized from Caregiver: The caregiver is abusive or neglectful and often responds in frightening or frightened ways towards the child.</li>
+
+		<li>Disorganized from Child: The child has no attaching behaviors, often appears dazed, confused, or apprehensive in presence of caregiver.</li>
+	</ul>	
+	
+	<div class="ch4Images">
+		<img src="includes/images/chapter4/footprints.png" alt="Foot print behaviors, adult and child representation">
+	</div>
+
+	<h4>Adult Behavior and Attachments</h4>
+	<p>After childhood the experience in care reflects in humans quite deeply. The second part of Attachment theory by Mary Ainsworth dives into the effects the attachment still holds over the adults behavior. The adult Attachment Behavior can be broken down into 4 sections: Autonomous/Secure, Dismissing, Resistant/Preoccupied, Unresolved/Disorganized.</p>
+	<ul class="solidCircle">
+		<li>Autonomous/Secure adult can discuss the past in consistent and coherent manners. They are able to recall both positive and negative aspects of their past.</li>
+
+		<li>Dismissing adults cannot recall parental interactions or minimize impact of parents on their development.</li>
+
+		<li>Resistant/Preoccupied adults are intensely focused on their parents, they tend to give confused and angry accounts or attachment experiences.</li>
+
+		<li>Unresolved/Disorganized adults generally have past traumatic experiences of loss or abuse and their descriptions of past may lack reasoning or sense.</li>	
+	</ul>
+
+	<p>When it comes to human nature it is important to understand who you are and how you respond to getting close to people. If your past was Autonomous/Secure you may have an easier time than others in terms of loving others, but regardless of how you were raised it is up to the adult to realize patterns from their past that may harm their relationships and to change them.</p>	
+	<!- Interesting Fact goes here ->
+
+	<div class="ch4Images">
+		<img src="includes/images/chapter4/shadows.png" alt="Shadows of people going in all different directions">
+	</div>
+
+
+</div>
 
 <!- Footer section ->
 <?php include 'includes/footer.php'; ?>
