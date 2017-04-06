77bed913f5f91f42763be63268ba20747cb67f61
davidfitz314 <davidfitz314@gmail.com>
2017-04-06 15:49:47 -0600

    chapter 9 content add & stylesheet update

--- chapter9.php
+++ chapter9.php
@@ -40,22 +40,214 @@
 
 <!- top page image ->
 <div class="chapterLogo">
-<img src="includes/images/medical_instruments.jpg" alt="change to default">
+<img src="includes/images/chapter9/condom.png" alt="Condom in shirt pocket, being touched by a hand">
 </div>
 
 <!- chapter text division ->
 <div class="chapter2Text">
 <br>
-<h2>Chapter 9: </h2>
-	<h3></h3>
-	</p></p>
+<h2>Chapter 9: Staying Safe During Sex</h2>
+	<h3>Safe Sex</h3>
+	</p>Safe sex is a way to combine the greatest pleasure while having the least amount of risk for contracting HIV and other sexuallt transmitted infections. Safe sex besides protecting a person can also help communication and trust between partners. There are two main groups to safe sex, the safest sex and regular safe sex:</p>
 
+	<p>The safest sex doesn’t involve penetration or the having genitals touch the partner, by doing this, STI’s have no access method from one person to another. A few methods for safest sex involve:</p>
 
+	<ul class="closedCircle">
+		<li>Fantasizing or having phone sex.</li>
 
+		<li>Touching one's own body  erotically (masturbation) or having each partner touch their own body at the same time (mutual masturbation).</li>
 
-	<div class="ch8Images">
-		<img src="includes/images/chapter8/" alt="">
+		<li>Caressing one’s partner using nonsexual massage.</li>
+
+		<li>Rubbing against one’s partner’s body while wearing clothes.</li>
+
+		<li>Kissing one’s partner everywhere except for the genital regions.</li>
+	</ul>
+
+	<p>On a daily basis many people choose safe sex and safest sex. The most used reasons are:</p>
+	<ul class="closedCircle">
+		<li>Safe sex prevents some STI’s.</li>
+
+		<li>Safe sex prevents pregnancies.</li>
+
+		<li>Finances, children can cost a lot and people are not prepared financially yet.</li>
+
+		<li>Safe sex builds trust among partners.</li>
+
+		<li>Safe sex helps build emotional bonds with partners.</li>
+	</ul>
+
+	<div class="ch9Images">
+		<img src="includes/images/chapter9/safesex.png" alt="2 safes in a sexual position">
 	</div>
+
+	<h3>Sexual Protection</h3>
+	<p>For the regular safe sex there are many methods used to protect both partners from STI’s and pregnancies:</p>
+	<ul class="closedCircle">
+		<li>Oral sex while using a condom, dental dam, or plastic wrap.</li>
+
+		<li>Vaginal sex with either a male or female condom.</li>
+
+		<li>Anal sex with either a male or female condom.</li>
+
+		<li>and more...</li>
+	</ul>
+
+	<p>Male oriented Protections consist of:</p>
+	<ul class="closedCircle">
+		<li>Condom made of either latex or nonlatex.</li>
+
+		<li>Vasectomy.</li>
+
+		<li>Withdrawal.</li>
+
+		<li>and more...</li>
+	</ul>
+
+	<p>Female oriented protections consist of:</p>
+	<ul class="closedCircle">
+		<li>Condom (latex or nonlatex).</li>
+
+		<li>The Pill.</li>
+
+		<li>The Diaphragm.</li>
+
+		<li>The cervical cap/femcap.</li>
+	
+		<li>IUD.</li>
+
+		<li>Contraceptive Implant.</li>
+
+		<li>Contraceptive Sponge.</li>
+
+		<li>Contraceptive Injections.</li>
+
+		<li>Vaginal Ring.</li>
+
+		<li>Emergency Contraceptives.</li>
+		<ul class="openCircle">
+			<li class="additalic"><strong>Example:</strong> Plan B pill.</li>
+		</ul>
+
+		<li>Sterilization</li>
+	</ul>
+
+	<h3>Nonprescription Protection Use</h3>
+	<p>A quick overview of how to use some non prescription protections is listed below:<p>
+	
+	<!- Image goes beside h4 header ->
+	<div class="ch9Images">
+		<img src="includes/images/chapter9/male.png" alt="Male condom full example">
+	</div>
+
+	<div class="chapter9MiniHeaders">
+		<h4 class="additalic">Male Condom</h4>	
+	</div>
+	<ol>
+		<li>Check the expiration date and then open the package carefully from the top corner, inspecting the condom for any damage.<li>
+
+		<li>Place the condom on the tip of the penis, pinching the condoms top balloon portion between your fingers (this bubble is used to provide extra space in the condom for when one ejaculates and needs to store the fluid in the condom without breaking).<li>
+
+		<li>While still pinching, unroll the remainder of the condom down the shaft of the penis until either the condom cannot be unrolled anymore or the penis is fully covered.<li>
+
+		<li>Let go of the tip of the condom.<li>
+
+		<li>With some condoms a lubricant is not provided, if the condom feels dry or not as wet as one is comfortable with, a lubricant can be used with the condom.<li>
+	</ol>
+
+	<!- Image goes beside h4 header ->
+	<div class="ch9Images">
+		<img src="includes/images/chapter9/female.png" alt="Female condom">
+	</div>
+
+	<div class="chapter9MiniHeaders">
+		<h4 class="additalic">Female Condom</h4>	
+	</div>
+	<ol>
+		<li>Check the expiration date and then open the package carefully from the top corner, inspecting the condom for any damage.<li>
+		
+		<li>Relax and get into a comfortable position in preparation for placing the condom inside of the vagina.<li>
+
+		<ul class="openCircle">
+			<li class="additalic">The female condom already comes lubricated but many still add even more lubricant or even a physician approved spermicide.</li>
+		</ul>
+
+		<li>Squeeze together the sides of the inner ring at the closed end of the condom and then slide the inner ring into the vagina.<li>
+
+		<li>Push the inner ring into the vagina as far as it will go or until the cervix being careful to make sure that it is not twisted.<li>
+
+		<li>Leave the condoms outer ring hanging about 1 inch outside the vagina and during sex remember to guide one’s partner’s penis into the outer ring of the condom.<li>
+	</ol>
+
+	<!- Image goes beside h4 header ->
+	<div class="ch9Images">
+		<img src="includes/images/chapter9/spermicide.png" alt="Bottle of spermicide">
+	</div>
+
+	<div class="chapter9MiniHeaders">
+		<h4 class="additalic">Spermicide</h4>
+	</div>
+	<p class="additalic">-These are simplified instructions for spermicide for more details please read the instructions that come with your product.-</p>
+	<ol>
+		<li>Check the expiration date and then open the cap of the spermicide.<li>
+
+		<li>Place the tip inside of the vaginal opening and squeeze the bottle to release the amount of spermicide as directed on the instructions that came with it.<li>
+			
+		<li>Remove the bottle from the vagina.<li>
+	</ol>
+
+	<!- Image goes beside or above h4 header ->
+	<div class="ch9Images">
+		<img src="includes/images/chapter9/exit.png" alt="Figures in bed thinking of withdrawing before ejaculation">
+	</div>
+	<div class="chapter9MiniHeaders">
+		<h4 class="additalic">Withdrawal</h4>	
+	</div>
+	<ol>
+		<li>Prior to ejaculation remove the penis completely from the vagina.<li>
+		<ul class="openCircle">
+			<li class="additalic">This method is only recommended for males who can clearly understand when they are about to ejaculate and can control themselves enough to leave the vagina at the correct moment.</li>
+		</ul>
+	</ol>
+
+	<p>Whether the protection being used is prescribed or non-prescription, they each have a timeframe for when they can be used and how often they can be used. Remember to always check the packaging and healthcare professionals for the time sensitive information and other related information for protection.</p>
+
+	<h3>Where to Acquire Protection</h3>
+	<p>At most grocery stores and gas stations in the US, a variety of non prescriptions protections like condoms, spermicides, and lubricants are available for purchase.</p>
+
+	<p>One may schedule a doctor’s appointment with an approved physician to get a prescription protection.</p>
+
+	<p>In the event that one is unable to financially purchase protections, there are still a few options available. Wellness Centers and Planned Parenthood both offer cheaper prescription protections if not free based on the amount of income one has. These places also usually offer a nonprescription condoms for free for all who just come in.</p>
+
+	<div class="ch9Images">
+		<img src="includes/images/chapter9/ring.png" alt="Pure by choice ring">
+	</div>
+
+	<h3>Abstinence</h3>
+	<p>Abstinence is a valid way to protect oneself from STI’s and pregnancy. Abstinence is the practice of restraining oneself from indulging in something.</p>
+
+	<p>Many people choose abstinence and each have their own reasons for it, but a few of the most common reasons are:</p>
+
+	<ul class="closedCircle">
+		<li>Waiting for the right person to be sexually active with.</li>
+
+		<li>Mourning the loss of a significant other.</li>
+
+		<li>Focusing on work or education.</li>
+
+		<li>Recovering from an illness.</li>
+
+		<li>Maintaining a moral or religious principle.</li>
+	</ul>
+
+<h4 class="additalic">Is abstinence right for you?</h4>
+
+	<ul class="closedCircle">
+		<li>The choice of abstinence or not is always up to the person. If one chooses yes or no then that is your answer. One should never let someone else make this choice for you, as this is your body and not theirs.</li>
+	</ul>
+
+<!- Links to ouside sexual protection centers here? ->
+
 </div>
 
 
