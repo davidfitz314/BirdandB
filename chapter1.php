c7d9152b3ec841f2e3190eb061440e6aefa617e5
davidfitz314 <davidfitz314@gmail.com>
2017-04-06 14:36:09 -0600

    updating menu's

--- chapter1.php
+++ chapter1.php
@@ -6,19 +6,19 @@
 <!- navigation side bar ->
 <nav class="floating-menu">
 <h2>Chapter 1: Overview </h2>
-<strong><a href="chapter2.php">ch2 Conception</a></strong>
-<strong><a href="chapter3.php">ch3 Human Body Development</a></strong>
-<strong><a href="chapter4.php">ch4 Human Nature and Psychological Dependencies</a></strong>
-<strong><a href="chapter5.php">ch5 What is Sex</a></strong>
-<strong><a href="chapter6.php">ch6 Sexuality</a></strong>
-<strong><a href="chapter7.php">ch7 Love vs Sex</a></strong>
-<strong><a href="chapter8.php">ch8 Orgasm</a></strong>
-<strong><a href="chapter9.php">ch9 Staying Safe During Sex</a></strong>
-<strong><a href="chapter10.php">ch10 Sexually Transmitted Infections-STDs</a></strong>
-<strong><a href="chapter11.php">ch11 Human Sexuality in the World</a></strong>
-<strong><a href="chapter12.php">ch12 Kinks and Fetishes</a></strong>
-<strong><a href="chapter13.php">ch13 Sexual Harassment, Assault, &amp; Rape</a></strong>
-<strong><a href="chapter14.php">ch14 Works Cited and External Resources</a></strong>
+<strong><a href="chapter2.php">ch2: Conception</a></strong>
+<strong><a href="chapter3.php">ch3: Human Body Development</a></strong>
+<strong><a href="chapter4.php">ch4: Human Nature and Psychological Dependencies</a></strong>
+<strong><a href="chapter5.php">ch5: What is Sex</a></strong>
+<strong><a href="chapter6.php">ch6: Sexuality</a></strong>
+<strong><a href="chapter7.php">ch7: Love vs Sex</a></strong>
+<strong><a href="chapter8.php">ch8: Orgasm</a></strong>
+<strong><a href="chapter9.php">ch9: Staying Safe During Sex</a></strong>
+<strong><a href="chapter10.php">ch10: Sexually Transmitted Infections-STDs</a></strong>
+<strong><a href="chapter11.php">ch11: Human Sexuality in the World</a></strong>
+<strong><a href="chapter12.php">ch12: Kinks and Fetishes</a></strong>
+<strong><a href="chapter13.php">ch13: Sexual Harassment, Assault, &amp; Rape</a></strong>
+<strong><a href="chapter14.php">ch14: Works Cited and External Resources</a></strong>
 </nav>
 <!- end navigation sidebar ->
 
--- chapter2.php
+++ chapter2.php
@@ -5,7 +5,7 @@
 
 <!- navigation side bar ->
 <nav class="floating-menu">
-<h2>Chapter 2 Conception</h2>
+<h2>Chapter 2: Conception</h2>
 <strong><a href="chapter1.php">ch1 Overview</a></strong>
 <ul>
 <strong>Chapter 2</strong>
@@ -22,18 +22,18 @@
 <a href="chapter2.php">Turning Full Term</a>
 </li>
 </ul>
-<strong><a href="chapter3.php">ch3 Human Body development</a></strong>
-<strong><a href="chapter4.php">ch4 Human Nature and Psychological Dependencies</a></strong>
-<strong><a href="chapter5.php">ch5 What is Sex</a></strong>
-<strong><a href="chapter6.php">ch6 Sexuality</a></strong>
-<strong><a href="chapter7.php">ch7 Love vs Sex</a></strong>
-<strong><a href="chapter8.php">ch8 Orgasm</a></strong>
-<strong><a href="chapter9.php">ch9 Staying Safe During Sex</a></strong>
-<strong><a href="chapter10.php">ch10 Sexually Transmitted Infections-STDs</a></strong>
-<strong><a href="chapter11.php">ch11 Human Sexuality in the World</a></strong>
-<strong><a href="chapter12.php">ch12 Kinks and Fetishes</a></strong>
-<strong><a href="chapter13.php">ch13 Sexual Harassment, Assault, &amp; Rape</a></strong>
-<strong><a href="chapter14.php">ch14 Works Cited and External Resources</a></strong>
+<strong><a href="chapter3.php">ch3: Human Body development</a></strong>
+<strong><a href="chapter4.php">ch4: Human Nature and Psychological Dependencies</a></strong>
+<strong><a href="chapter5.php">ch5: What is Sex</a></strong>
+<strong><a href="chapter6.php">ch6: Sexuality</a></strong>
+<strong><a href="chapter7.php">ch7: Love vs Sex</a></strong>
+<strong><a href="chapter8.php">ch8: Orgasm</a></strong>
+<strong><a href="chapter9.php">ch9: Staying Safe During Sex</a></strong>
+<strong><a href="chapter10.php">ch10: Sexually Transmitted Infections-STDs</a></strong>
+<strong><a href="chapter11.php">ch11: Human Sexuality in the World</a></strong>
+<strong><a href="chapter12.php">ch12: Kinks and Fetishes</a></strong>
+<strong><a href="chapter13.php">ch13: Sexual Harassment, Assault, &amp; Rape</a></strong>
+<strong><a href="chapter14.php">ch14: Works Cited and External Resources</a></strong>
 </nav>
 <!- end navigation sidebar ->
 
