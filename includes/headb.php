102fc3e3f8d1408c6aaf5a9832603eba05869022
davidfitz314 <davidfitz314@gmail.com>
2017-04-06 21:31:42 -0600

    updating stylesheet

--- includes/headb.php
+++ includes/headb.php
@@ -23,7 +23,7 @@
 		</div>
 		<div class="searchBarC">
 			<form>
-				<input type="text" name="search" placeholder="Search Bar (coming soon)..." size="15">
+				<input type="text" name="search" placeholder="Search Bar (coming soon)..." size="20">
 			</form>	  
 		</div>
 	</div>
