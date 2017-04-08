f640b64305506144bb089c057fa3ee57b1be3afc
davidfitz314 <davidfitz314@gmail.com>
2017-04-07 19:23:09 -0600

    inserting content to chapter 12 kinks and fetishes

--- chapter12.php
+++ chapter12.php
@@ -20,16 +20,13 @@
 <ul>
 <strong>Chapter 12</strong>
 <li>
-<a href="chapter12.php">default</a>
+<a href="chapter12.php">What is a Fetish &amp; Kink</a>
 </li>
 <li>
-<a href="chapter12.php">default</a>
+<a href="chapter12.php">Common Fetishes and Kinks</a>
 </li>
 <li>
-<a href="chapter12.php">default</a>
-</li>
-<li>
-<a href="chapter12.php">default</a>
+<a href="chapter12.php">Non-Sexual Fetishes</a>
 </li>
 </ul>
 
@@ -40,21 +37,39 @@
 
 <!- top page image ->
 <div class="chapterLogo">
-<img src="includes/images/medical_instruments.jpg" alt="change to default">
+<img src="includes/images/chapter12/kinky.png" alt="Female lower half shadow wearing a brown leather skirt">
 </div>
 
 <!- chapter text division ->
-<div class="chapter2Text">
+<div class="chapter12Text">
 <br>
-<h2>Chapter 12: </h2>
-	<h3></h3>
+<h2>Chapter 12: Kinks and Fetishes</h2>
+	<h3>What is a Fetish &amp; Kink</h3>
+	</p>A fetish or kink is a form of sexual desire in which gratification is linked to an abnormal degree to a particular object, item of clothing, part of the body, or etc. Fetishes are generally an excessive and irrational devotion or commitment to a particular thing. They can be an inanimate object worshiped for its supposed magical powers, or because it is considered to be inhabited by a spirit or something else.</p>
+
+	<h4>Do I Have to Like Their Fetish</h4>
+	<p>Each person has the chance to have a fetish, whether it be to specific body parts, clothing, or more. When interacting with someone who has expressed their fetish to you verbally it is important to understand their opinion, but that does not mean you have to also engage in their fetish.</p>
+
+	<p>To the other person a fetish can be a healthy way to relieve sexuality, sensuality, and sexual desires. Accepting someone’s fetish is simply a way of accepting another person as a friend, and while they may enjoy their fetish, you are never required to do everything they want to. Instead be their friends, but make clear where your stance is on the subject.</p>
+
+	<div class="ch12Images">
+		<img src="includes/images/chapter12/speakout.png" alt="Black and white sketch of a person at a podium">
+	</div>
+
+	<h4>How Common are Fetishes?</h4>
+	<p class="addItalic">According to a survey done by Ann Summers in the UK approximately 75% of people have at least one fetish.<br>In an article from Women’s Health by Debra W. Soh, the amount of people with a fetish is 1 in 6. While more stats on sexual fetishes may be hard to find, the Mark Allen Thorton blog published a statistic evaluating the authors of over 2900 books, and graphed them out based on the age group focuses the books were written for. In his studies fetishes were common and varied by different age groups.</p>
+
+	<h3>Common Fetishes and Kinks</h3>
+	</p></p>
+
+	<h3>Non-Sexual Fetishes</h3>
 	</p></p>
 
 
 
 
-	<div class="ch8Images">
-		<img src="includes/images/chapter8/" alt="">
+	<div class="ch12Images">
+		<img src="includes/images/chapter12/" alt="">
 	</div>
 </div>
 
