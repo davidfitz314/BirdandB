df97cc9923f708d85d982f7439e1c7c97bc3ab5c
davidfitz314 <davidfitz314@gmail.com>
2017-04-11 14:00:18 -0600

    entering ch14 content

--- chapter14.php
+++ chapter14.php
@@ -48,99 +48,99 @@
 
 	<div class="sectionContainer">
 		<div class="ch14group1">
-			<p></p>
+			<p>Agrawal, N. (2015, December 18). 21 Harrowing Stories Of Sexual Harassment On The Job. Retrieved April 04, 2017, from http://www.huffingtonpost.com/entry/21-harrowing-stories-of-sexual-harassment-on-the-job_us_566f39b1e4b0fccee16f84d8</p>
 		</div>
 
 		<div class="ch14group2">
-			<p></p>
+			<p>Alfred Kinsey. (2017, February 15). Retrieved February 16, 2017, from https://en.wikipedia.org/wiki/Alfred_Kinsey</p>
 		</div>
 
 		<div class="ch14group3">
-			<p></p>
+			<p>American Teens' Sexual and Reproductive Health. (2016, November 02). Retrieved February 01, 2017, from https://www.guttmacher.org/fact-sheet/american-teens-sexual-and-reproductive-health</p>
 		</div>
 	</div>
 
 	<div class="sectionContainer">
 		<div class="ch14group1">
-			<p></p>
+			<p>Asia-Pacific | Pick up a penguin. (1998, February 26). Retrieved March 11, 2017, from http://news.bbc.co.uk/2/hi/asia-pacific/60302.stm</p>
 		</div>
 
 		<div class="ch14group2">
-			<p></p>
+			<p>AskAlice. (n.d.). Stages of male sexual response. Retrieved February 06, 2017, from http://goaskalice.columbia.edu/answered-questions/stages-male-sexual-response</p>
 		</div>
 
 		<div class="ch14group3">
-			<p></p>
+			<p>Basic Emotions--Plutchik. (1998, April). Retrieved March 02, 2017, from http://www.personalityresearch.org/basicemotions/plutchik.html</p>
 		</div>
 	</div>
 <!- 3 ->
 	<div class="sectionContainer">
 		<div class="ch14group1">
-			<p></p>
+			<p>Blott, U. (2016, November 30). Which body type do men REALLY prefer? We fit 10 volunteers with eye-tracking devices to find out whether they like curvy, athletic or slim women... so who got the most attention? . Retrieved February 03, 2017, from http://www.dailymail.co.uk/femail/article-3935212/MailOnline-discovers-body-type-men-like-best.html</p>
 		</div>
 
 		<div class="ch14group2">
-			<p></p>
+			<p>Body Changes After Sex. (2015, September 22). Retrieved February 06, 2017, from http://www.md-health.com/Body-Changes-After-Sex.html</p>
 		</div>
 
 		<div class="ch14group3">
-			<p></p>
+			<p>Brocail, M. (2015, July 02). STDs and The Law: Do You Have To Tell Your Partner? Retrieved February 16, 2017, from https://www.stdcheck.com/blog/stds-and-the-law/</p>
 		</div>
 	</div>
 
 	<div class="sectionContainer">
 		<div class="ch14group1">
-			<p></p>
+			<p>Brodie, R. (n.d.). Mary Ainsworth and Attachment Theory. Retrieved March 02, 2017, from http://www.childdevelopmentmedia.com/articles/mary-ainsworth-and-attachment-theory/</p>
 		</div>
 
 		<div class="ch14group2">
-			<p></p>
+			<p>Brown, E. N. (2014, November 11). Kinky Sex Survey Says: You're Not as Deviant as You Think. Retrieved February 16, 2017, from http://reason.com/blog/2014/11/11/kinky-sex-survey-finds-everybodys-weird</p>
 		</div>
 
 		<div class="ch14group3">
-			<p></p>
+			<p>Bruk, D. (2017, January 06). How 1 Man's Body Was Photoshopped to Meet 19 Different "Ideal" Beauty Standards Around the World. Retrieved February 04, 2017, from http://www.cosmopolitan.com/lifestyle/news/a53831/global-beauty-standards-men/</p>
 		</div>
 	</div>
 
 	<div class="sectionContainer">
 		<div class="ch14group1">
-			<p></p>
+			<p>Buckley, S. (2016, February 27). China's high-speed sexual revolution. Retrieved February 16, 2017, from http://www.bbc.com/news/magazine-35525566</p>
 		</div>
 
 		<div class="ch14group2">
-			<p></p>
+			<p>Burton, N. (2017, February 02). Long Necks, Super-Sized Lips, And Other Bizarre Beauty Standards From Around the World. Retrieved February 04, 2017, from http://www.cosmopolitan.com/style-beauty/g3279/weird-beauty/</p>
 		</div>
 
 		<div class="ch14group3">
-			<p></p>
+			<p>Dallas, M. E. (2016, October 14). The 7 Most Common Sexually Transmitted Diseases | Healthgrades. Retrieved February 16, 2017, from https://www.healthgrades.com/conditions/7-most-common-sexually-transmitted-diseases</p>
 		</div>
 	</div>
 
 	<div class="sectionContainer">
 		<div class="ch14group1">
-			<p></p>
+			<p>Duquette, W. S. (2012, November 11). The Ideal Male Physique - What girls want & what guys want to be. Retrieved February 03, 2017, from http://foxhoundstudio.com/blog/fitness-lifestyle/the-ideal-male-physique-%E2%80%94-what-girls-want-want-guys-want-to-be/</p>
 		</div>
 
 		<div class="ch14group2">
-			<p></p>
+			<p>Emery, L. R. (2016, October 18). How Many People Have A Sexual Fetish? It's More Common Than You Think, But It's Still Stigmatized. Retrieved February 16, 2017, from https://www.bustle.com/articles/190171-how-many-people-have-a-sexual-fetish-its-more-common-than-you-think-but-its-still</p>
 		</div>
 
 		<div class="ch14group3">
-			<p></p>
+			<p>Eve, Raymond A., and Donald G. Renslow. AN EXPLORATORY ANALYSIS OF PRIVATE SEXUAL BEHAVIORS AMONG COLLEGE STUDENTS: SOME IMPLICATIONS FOR A THEORY OF CLASS DIFFERENCES IN SEXUAL BEHAVIOR. 1st ed. Vol. 8. N.p.: Scientific Journal Publishers, 1980. Print. Social Behavior and Personality: an international journal.</p>
 		</div>
 	</div>
 
 	<div class="sectionContainer">
 		<div class="ch14group1">
-			<p></p>
+			<p>Female Puberty | SexInfo Online. (2015, April 21). Retrieved January 20, 2017, from http://www.soc.ucsb.edu/sexinfo/article/female-puberty</p>
 		</div>
 
 		<div class="ch14group2">
-			<p></p>
+			<p>Ferraiuolo, J. (2014, June 04). 10 of the Most Popular Sexual Fetishes. Retrieved February 16, 2017, from http://www.therichest.com/rich-list/most-popular/10-of-the-most-popular-sexual-fetishes/</p>
 		</div>
 
 		<div class="ch14group3">
-			<p></p>
+			<p>Ferro, S. (2013, January 28). Did Penicillin Kickstart The Sexual Revolution? Retrieved February 16, 2017, from http://www.popsci.com/science/article/2013-01/did-penicillin-kickstart-sexual-revolution</p>
 		</div>
 	</div>
 
