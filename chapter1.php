9f23ab2012fd2afab8796aec24e33692cbdf9a56
davidfitz314 <davidfitz314@gmail.com>
2017-04-11 13:10:51 -0600

    entering ch1 content

--- chapter1.php
+++ chapter1.php
@@ -5,7 +5,7 @@
 
 <!- navigation side bar ->
 <nav class="floating-menu">
-<h2>Chapter 1: Overview </h2>
+<strong><a href="chapter1.php">Chapter 1:</a></strong>
 <strong><a href="chapter2.php">ch2: Conception</a></strong>
 <strong><a href="chapter3.php">ch3: Human Body Development</a></strong>
 <strong><a href="chapter4.php">ch4: Human Nature and Psychological Dependencies</a></strong>
@@ -24,15 +24,129 @@
 
 <!- top page image ->
 <div class="chapterLogo">
-<img src="includes/images/medical_instruments.jpg" alt="change to default">
+<img src="includes/images/chapter1/honeycomb.png" alt="Black and white honey comb">
 </div>
 
 <!- chapter text division ->
-<div class="chapter2Text">
+<div class="chapter1Text">
 <br>
-<strong><p>Under construction </p></strong>
-</div>
+<h2>Chapter 1: Introduction</h2>
+	<h3>Chapters Overview</h3>
+	<p>Human sexuality from a biological, psychological, and educational perspective.</p>
+	<p>A place where all types of people can come and learn the basics about life’s most confusing and hard to talk about questions.</p>
+	<p class="tabbed">“<i>A library is a place where you can lose your innocence without losing your virginity.</i>” ― Germaine Greer</p>
+	<p class="tabbed">“<i>I knew I was different. I thought that I might be gay or something because I couldn't identify with any of the guys at all. None of them liked art or music. They just wanted to fight and get laid. It was many years ago but it gave me this real hatred for the average American macho male.</i>” ― Kurt Cobain</p>
 
+
+	<div class="sectionContainer">
+		<div class="ch1group1">
+			<h3>02</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Conception</h4>			
+			</div>
+			<p>The action of conceiving a child from fertilization to full term development and size comparisons per developmental weeks.</p>
+		</div>
+
+		<div class="ch1group2">
+			<h3>03</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Human Body Development</h4>			
+			</div>
+			<p>Human biological development for male and female sexes, from pre-puberty through post-puberty. Includes the basics of sexual anatomy primary and secondary developments, other biological development (hair, acne, etc...) and hormonal changes.</p>
+		</div>
+			
+		<div class="ch1group3">
+			<h3>04</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Human Nature and Psychological Dependencies</h4>			
+			</div>
+			<p>Human Nature and the distinguishing characteristics; including ways of thinking, feeling, and acting. The psychological patterns related to child development from different parenting types and their effect as the child becomes an adult.</p>
+		</div>
+	</div>
+
+	<div class="sectionContainer">
+		<div class="ch1group1">
+			<h3>05</h3>
+			<div class="chapter1chaptersheader">
+				<h4>What is Sex</h4>			
+			</div>
+			<p>Definition of sex and sexual acts, explanations of different sexual act
+types including the solitary and group use of objects to achieve sexual performance.</p>
+			<p>A quick overview of the percentage of people having sex and sexual activities.</p>
+		</div>
+
+		<div class="ch1group2">
+			<h3>06</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Sexuality</h4>			
+			</div>
+			<p>Definitions and explanations of sexual identity, gender, sexual preference, and sex in regards to biology.</p>
+			<p>Sex appeal and beauty in both a local culture and world wide.</p>
+		</div>
+
+		<div class="ch1group3">
+			<h3>07</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Love VS Sex</h4>			
+			</div>
+			<p>Defining love and sex and providing a comparison and contrast for the two.</p>
+			<p>Descriptions of biological reactions to love and sex from both the male and female perspective.</p>
+			<p>Overview of the psychological reactions produced from having sex or being in love.</p>
+		</div>
+	</div>
+
+	<div class="sectionContainer">
+		<div class="ch1group1">
+			<h3>08</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Orgasm</h4>			
+			</div>
+			<p></p>
+		</div>
+
+		<div class="ch1group2">
+			<h3>09</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Staying Safe During Sex</h4>			
+			</div>
+			<p></p>
+		</div>
+
+		<div class="ch1group3">
+			<h3>10</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Sexually Transmitted Infections (STDs)</h4>			
+			</div>
+			<p></p>
+		</div>
+	</div>
+
+	<div class="sectionContainer">
+		<div class="ch1group1">
+			<h3>11</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Human Sexuality in the World</h4>			
+			</div>
+			<p></p>
+		</div>
+
+		<div class="ch1group2">
+			<h3>12</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Kinks and Fetishes</h4>			
+			</div>
+			<p></p>
+		</div>
+
+		<div class="ch1group3">
+			<h3>13</h3>
+			<div class="chapter1chaptersheader">
+				<h4>Sexual Harassment, Assault, & Rape</h4>			
+			</div>
+			<p></p>
+		</div>
+	</div>
+</div>
 <div class="expandtest">
 </div>
 
