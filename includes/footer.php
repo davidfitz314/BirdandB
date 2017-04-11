af77f06ffbc30256df8027685e75dc5cfaff015d
davidfitz314 <davidfitz314@gmail.com>
2017-04-11 12:43:12 -0600

    updating footer for all chapters

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <p>David Chen Fitzgerald</p>
 <p>Jamie Chapman</p>
 <p><a href="chapter14.php">Resources</a></p>
-<p><a href="index.php">Contact(under construction)</a></p>
+<p><a href="index.php">Contact (under construction)</a></p>
 <!- two images facebook and linked in go here ->
 <div class="footerImages">
 
