bdb7d9f553c0a195e53926ef82df53bdeaf5056b
davidfitz314 <davidfitz314@gmail.com>
2017-04-12 20:55:08 -0600

    deleted media query

--- includes/chapHeader.php
+++ includes/chapHeader.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="includes/styles.css">
 </head>
-	
+
 <body>
 <div class="centered-wrapper">
 	<div class="cNavi">
