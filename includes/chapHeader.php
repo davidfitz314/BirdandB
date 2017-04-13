7f0f7242d7625ea453e90481bc6685034262adb0
davidfitz314 <davidfitz314@gmail.com>
2017-04-13 12:19:11 -0600

    testing new media query

--- includes/chapHeader.php
+++ includes/chapHeader.php
@@ -7,7 +7,7 @@
   <meta name="keywords" content="HTML,CSS,XML,PHP,JavaScript">
   <meta name="author" content="David ChenFitzgerald">
   <meta name="author" content="Jamie Chapmans">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=0.25">
   <link rel="stylesheet" type="text/css" href="includes/styles.css">
 </head>
 
