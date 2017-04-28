39cb6a2a325a8a4786b9b3c136095fbfdc4de041
davidfitz314 <davidfitz314@gmail.com>
2017-04-27 20:14:27 -0600

    adding in copyright for jamies images

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
 <footer><p>&copy; 2017</p>
 <p>David Chen Fitzgerald</p>
 <p>Jamie Chapman</p>
+<p class="smallertext">(All Images &copy; Jamie Chapman)</p>
 <p><a href="chapter14.php">Resources</a></p>
 <p><a href="index.php">Contact (under construction)</a></p>
 <!- two images facebook and linked in go here ->
