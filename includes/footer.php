5b9d9f31bc37ad01017f3cf43f3906dab5261ac9
davidfitz314 <davidfitz314@gmail.com>
2017-04-27 20:17:42 -0600

    updating footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer><p>&copy; 2017</p>
 <p>David Chen Fitzgerald</p>
-<p>Jamie Chapman</p>
-<p class="smallertext">(All Images &copy; Jamie Chapman)</p>
+<p>Jamie Chapman</br>
+<span class="shrinkText">(All Images &copy; Jamie Chapman)</span></p>
 <p><a href="chapter14.php">Resources</a></p>
 <p><a href="index.php">Contact (under construction)</a></p>
 <!- two images facebook and linked in go here ->
